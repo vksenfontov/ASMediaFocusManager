3e236f58b29035b00c08cab338a4dcc8c433d548
Luke Durrant <contact@lukedurrant.com>
2016-07-29 10:57:27 +1000

    increase to 2.0

--- ASMediaFocusManager/ASImageScrollView.m
+++ ASMediaFocusManager/ASImageScrollView.m
@@ -154,7 +154,7 @@
     }
 
     CGSize boundsSize = self.bounds.size;
-    CGFloat maxScale = 1.5;
+    CGFloat maxScale = 2.0;
     
     // calculate min/max zoomscale
     CGFloat xScale = boundsSize.width  / _imageSize.width;    // the scale needed to perfectly fit the image width-wise
